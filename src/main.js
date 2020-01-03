06eb0a67e15712730d6de6fc81625e8126de0b2c
mingzai-zai <mingzai_2020@163.com>
2020-01-03 11:18:49 +0800

    头条准备工作完成

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// 引入全局样式
+import '@/styles/reset.css'
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
