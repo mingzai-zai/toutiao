d12559536e7b5604834af6b6dc3f37f02079ac7a
mingzai-zai <mingzai_2020@163.com>
2020-01-03 18:06:10 +0800

    文本框失焦的时候，使用vant轻提示，当然还有很多参数可以配置

--- src/components/input.vue
+++ src/components/input.vue
@@ -1,48 +1,55 @@
 <template>
-  <input type="text" class="put" @input="gai" :class="{'green':flag,'red':!flag,}" @blur='tips'>
+  <input
+    type="text"
+    class="put"
+    @input="gai"
+    :class="{ green: flag, red: !flag }"
+    @blur="tips"
+  />
 </template>
 
 <script>
 export default {
-    props:['rules','msg'],
-    methods:{
-        gai(event) {
-            let value = event.target.value;
-            if(this.rules && this.rules.test(value)) {
-                this.flag=true
-            }else{
-                this.flag=false;
-            }
-            this.$emit('input',value)
-        },
-        tips(){
-            if(!this.flag) {
-                console.log(this.msg || '不正确·')
-            }
-        }
+  props: ["rules", "msg"],
+  methods: {
+    gai(event) {
+      let value = event.target.value;
+      if (this.rules && this.rules.test(value)) {
+        this.flag = true;
+      } else {
+        this.flag = false;
+      }
+      this.$emit("input", value);
     },
-    data () {
-        return {
-            flag:true,
-        }
+    tips() {
+      if (!this.flag) {
+        // console.log(this.msg || '不正确·')
+        this.$toast.fail({message:this.msg || '不正确·',duration:4000});
+      }
     }
-}
+  },
+  data() {
+    return {
+      flag: true
+    };
+  }
+};
 </script>
 
-<style>
-.put{
-    width: 318*100vm/360;
-    height: 40px;
-    line-height: 60px;
-    outline: 0;
-    border: 0;
-    border-bottom: 2px solid #000;
-    font-size: 25px
+<style lang="less" scoped>
+.put {
+  width: 318 * 100vm/360;
+  height: 40px;
+  line-height: 60px;
+  outline: 0;
+  border: 0;
+  border-bottom: 2px solid #000;
+  font-size: 25px;
 }
-.green{
-    border-bottom: 2px solid rgb(13, 202, 54);
+.green {
+  border-bottom: 2px solid rgb(13, 202, 54);
 }
-.red{
-    border-bottom: 2px solid red;
+.red {
+  border-bottom: 2px solid red;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from '@/router/index.js'
 // 引入全局样式
 import '@/styles/reset.css'
+import { Toast } from 'vant'
+Vue.use(Toast)
 Vue.config.productionTip = false
 
 new Vue({
