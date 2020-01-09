f4f9bbfb8393748d840d3f5e21e23193480ba60c
mingzai-zai <mingzai_2020@163.com>
2020-01-09 12:18:08 +0800

    刚刚的是关注，收藏列表没有完成删除swipecell的

--- src/views/Mystar.vue
+++ src/views/Mystar.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="mystar">
-      <myheader title='我的关注'>
+      <myheader title='我的收藏'>
           <span slot="left" class="iconfont iconjiantou2" @click="$router.back()"></span>
       </myheader>
-      <articles></articles>
+      <articles :post='e' v-for="e in starlist" :key='e.id' @click="$router.push({path:`/articleDetail/${e.id}`})"></articles>
   </div>
 </template>
 
 <script>
 import myheader from '@/components/header.vue'
 import articles from '@/components/article.vue'
+import {userStarList} from '@/apis/user.js'
 export default {
     components:{
         myheader,articles
+    },
+    data () {
+        return {
+            starlist:[],
+        }
+    },
+   async mounted () {
+        let res = await userStarList();
+        console.log(res);  
+        this.starlist=res.data.data
     }
 }
 </script>
